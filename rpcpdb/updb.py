4e25147d4fc9baccc4a4fc66bead523c6abaff64
Ben Bass <benbass@codedstructure.net>
2012-05-12 21:40:35 +0100

    add ignore_count parameter

--- rpcpdb/updb.py
+++ rpcpdb/updb.py
@@ -4,7 +4,12 @@
 from __future__ import with_statement
 import inspect
 
-import pdb, socket, sys, os, tempfile
+import pdb
+import socket
+import sys
+import os
+import tempfile
+
 
 class UPdb(pdb.Pdb):
     def __init__(self, sock_path, level=0, force=False):
@@ -59,19 +64,23 @@ class UPdb_mixin(object):
     # (function, unix_socket_path) pairs.
     _updb_debug_func = {}
 
-    def debug_func(self, f, once=True, force=True, match_criteria=None):
+    def debug_func(self, f, once=True, force=True,
+                   match_criteria=None, ignore_count=0):
         """
         @param f: function name
         @param once: one-shot debug - undebug after first hit
         @param force: override any existing unix socket
+        @param match_criteria: dictionary of param name/value matches
+        @param ignore_count: don't trigger for ignore_count matches
         @return: path to debug socket
         """
         if f in self._updb_debug_func:
             return self._updb_debug_func[f][1]
         else:
             func = getattr(self, f)
-            pdb_sock_path = "%s/pdb_sock"%tempfile.mkdtemp(prefix='updb_')
+            pdb_sock_path = "%s/pdb_sock" % tempfile.mkdtemp(prefix='updb_')
             self._updb_debug_func[f] = (func, pdb_sock_path)
+
             def arg_match(o, k):
                 """
                 TODO: currently this will require an exact match on
@@ -87,16 +96,22 @@ class UPdb_mixin(object):
                     if key in called_with and called_with[key] == val:
                         return True
                 return False
-            def _(*o, **k):
-                if arg_match(o,k):
-                    if once:
-                        self.undebug_func(f)
-                    with UPdb(pdb_sock_path, level=1, force=force):
-                        return func(*o, **k)
-                else:
-                    # we're not debugging you, this time...
-                    return func(*o, **k)
-            setattr(self, f, _)
+
+            def debug_check(*o, **k):
+                if arg_match(o, k):
+                    debug_check._ignore_count -= 1
+                    if debug_check._ignore_count <= 0:
+                        if once:
+                            self.undebug_func(f)
+                        with UPdb(pdb_sock_path, level=1, force=force):
+                            return func(*o, **k)
+                # we're not debugging you, this time...
+                return func(*o, **k)
+            # use a function attribute to store the skip count;
+            # decrement this on each matched call. Trigger at <= 0
+            debug_check._ignore_count = ignore_count
+
+            setattr(self, f, debug_check)
             return pdb_sock_path
 
     def undebug_func(self, f):
